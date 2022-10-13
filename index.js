fd908aec4f34c8d8cb0f555e97e816d1d765421a
Mark Shkreli <m.shkreli96@icloud.com>
2022-10-13 17:43:51 -0400

    commit tree 0f8c19068552b9c949dbfb22d3d6c1116eb90a72

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const paragraph = document.getElementById('text');
+
+function loadEvent() {
+    paragraph.textContent = 'This is really cool!';
+}
+
+document.addEventListener('DOMContentLoaded', loadEvent);
